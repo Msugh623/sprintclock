2b0514a1b4761ebaa9372a4b5ccd5fc5965cf267
Msugh623 <iternenge469@gmail.com>
2024-10-02 17:30:27 +0100

    main

--- server.js
+++ server.js
@@ -1,14 +1,16 @@
 const express = require("express");
-const app = express()
-const port = process.env.PORT || 3001
-app.use(express.static('./public'))
+const getDirname = require("../../dirname");
+const clockApp = express()
+const path = require('path')
+
+clockApp.use(express.static(path.join(getDirname(), 'sites', 'clock', 'public')))
 
 const operators = {
     '-': (a, b) => a - b,
     '+': (a, b) => a + b
 }
 
-app.get('/datetime', async (req, res) => {
+clockApp.get('/datetime', async (req, res) => {
     const tz = req.query.tz || 'GMT'
     const operator = tz.split('GMT')[0] || '+'
     const tzUtc = tz.split('GMT')[1]
@@ -19,21 +21,4 @@ app.get('/datetime', async (req, res) => {
     res.json(Number(prsDate))
 })
 
-app.listen(port, () => {
-    console.log(`Server Runnin on port: ${port}`)
-
-    setInterval(() => {
-        setTimeout(() => {
-            (async () => {
-                try {
-                    const _ = await fetch('https://sprintclock.onrender.com', {
-                        method: 'GET',
-                    })
-                    console.log(`Ping: ${(new Date()).toString()}`)
-                } catch (err) {
-                    console.error(`PINGERROR: ${err}`)
-                }
-            })()
-        }, [Math.floor(Math.random() * 1000 * 20)])
-    }, 1000 * 60 * 120);
-})
+module.exports = { clockApp }
